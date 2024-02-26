0a03f227720cc4e6275530231e9e80a3391226e8
GabrielGarayalde <91076879+GabrielGarayalde@users.noreply.github.com>
2024-02-26 17:32:48 +0100

    more partial updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ export default {
   theme: {
     extend: {
       colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        
         primary: "#172554", // Example: Orange 500 from Material colors
+        accent: "#ea580c",
       },
     },
   },
